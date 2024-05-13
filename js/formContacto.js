011f0c7e209e266d0b63aaf40a0a17ecd91619a1
tadeo <tadeoacosta14@gmail.com>
2024-05-12 23:57:03 -0300

    Agregamos el metodo para guardarlo en el local storage

--- js/formContacto.js
+++ js/formContacto.js
@@ -2,9 +2,12 @@ const handleEnviar = document.querySelector('#handleEnviar');
 
 handleEnviar.addEventListener('click', addUser);
 
+const nombres = [];
+
 function addUser() {
 
     const nombreApellido = document.querySelector('#nombreApellido').value;
-    console.log(nombreApellido);
+    nombres.push(nombreApellido);
+    localStorage.setItem('nombres', JSON.stringify(nombres));
 }
 
